a2556a0354685acd7f4b451da603bbc55a35dc1d
Aditya Shukla <adityashukla74@gmail.com>
2023-09-01 23:53:59 +0530

    filtering working

--- src/Components/ComplexTable.jsx
+++ src/Components/ComplexTable.jsx
@@ -7,7 +7,8 @@ import {
   TableHead, 
   TableRow, 
   Paper,
-  TextField
+  TextField,
+  Button,
 } from '@mui/material';
 import CircularWithValueLabel from './Spinner';
 
@@ -16,9 +17,11 @@ function TableComponent() {
   const [loading, setLoading] = useState(true);
   const [sortConfig, setSortConfig] = useState({ key: null, direction: 'asc' });
   const [filterText, setFilterText] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10; // Change this value as needed
 
   useEffect(() => {
-    fetch('https://api.npoint.io/8d0109c35278f342992a', {
+    fetch(' https://api.npoint.io/ddd3b1d5c47816f77e9c', {
       method: 'GET',
       headers: {
         'Access-Control-Allow-Origin': 'http://localhost:3000', 
@@ -62,7 +65,7 @@ function TableComponent() {
     return data;
   };
 
-  const handleFilterChange = (e) => { // Step 4: Event handler for filter input
+  const handleFilterChange = (e) => {
     setFilterText(e.target.value);
   };
 
@@ -72,6 +75,9 @@ function TableComponent() {
       value.toString().toLowerCase().includes(filterText.toLowerCase())
     );
   });
+
+  const maxPage = Math.ceil(filteredData.length / itemsPerPage);
+
   const renderTableHeader = () => (
     <TableRow>
       {tableHeaders.map((header) => (
@@ -86,16 +92,23 @@ function TableComponent() {
     </TableRow>
   );
 
-  const renderTableRows = () => (
-    filteredData.map((row) => (
+  const renderTableRows = () => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
+    return filteredData.slice(startIndex, endIndex).map((row) => (
       <TableRow key={row.id}>
         {tableHeaders.map((header) => (
           <TableCell key={header}>{row[header]}</TableCell>
         ))}
       </TableRow>
-    ))
-  );
-  
+    ));
+  };
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
   if (loading) {
     return <CircularWithValueLabel />;
   }
@@ -103,21 +116,34 @@ function TableComponent() {
   return (
     <div>
       <TextField 
-        label='filter table'
+        label='Filter table'
         variant='outlined'
-        value= {filterText}
+        value={filterText}
         onChange={handleFilterChange}
         fullWidth
-        style={{marginBottom: '20px'}}
+        style={{ marginBottom: '20px' }}
       />
       <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>{renderTableHeader()}</TableHead>
-        <TableBody>{renderTableRows()}</TableBody>
-      </Table>
-    </TableContainer>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>{renderTableHeader()}</TableHead>
+          <TableBody>{renderTableRows()}</TableBody>
+        </Table>
+      </TableContainer>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+        <Button
+          disabled={currentPage === 1}
+          onClick={() => handlePageChange(currentPage - 1)}
+        >
+          Previous
+        </Button>
+        <Button
+          disabled={currentPage === maxPage}
+          onClick={() => handlePageChange(currentPage + 1)}
+        >
+          Next
+        </Button>
+      </div>
     </div>
-    
   );
 }
 
